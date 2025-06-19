4bd75264e6eb88434bfc3869f16ef6d3d6d4bf38
h9p <mhumam99@gmail.com>
2025-06-19 12:34:49 +0300

    Update web.rb

--- web.rb
+++ web.rb
@@ -1,54 +1,77 @@
+# encoding: UTF-8
 
-#Encoding: UTF-8
-
-
-#require libs
 require 'json'
-require 'rubygems'
 require 'telegram/bot'
 require 'yaml/store'
-require "colorize"
+require 'colorize'
 require 'securerandom'
 
+# Load config
+config_path = 'config.json'
+@config = JSON.parse(File.read(config_path))
+V = @config["Version"]
 
-
-#Config file
-@ConfigFile = File.read('config.json')
-@config = JSON.parse(@ConfigFile)
-
-#Game database
+# Databases
 db = YAML::Store.new('Game.yml')
 bd = YAML::Store.new('banned.yml')
 cn = YAML::Store.new('clans.yml')
 
-#bot token
+# Token
 token = @config["Token"]
 
-V = @config["Version"]
-
-#game start
 Telegram::Bot::Client.run(token) do |bot|
-	puts "#{@config["BotName"]} #{V} تم تشغيل بوت اللعبة بنجاح".on_yellow
-	begin
-		bot.listen do |message|
-			#require bot files
-			bd.transaction do
-				db.transaction do
-					cn.transaction do
-						eval(File.read("./plugins/setting.rb"))
-						eval(File.read("./plugins/levels.rb"))
-						eval(File.read("./plugins/attack.rb"))
-						eval(File.read("./plugins/store.rb"))
-						eval(File.read("./plugins/support.rb"))
-					end
-				end
-			end
-		end
-	rescue Telegram::Bot::Exceptions::ResponseError => e
-    	retry
-	end
+  puts "#{@config["BotName"]} #{V} تم تشغيل بوت اللعبة بنجاح".on_yellow
+
+  begin
+    bot.listen do |message|
+      bd.transaction do
+        db.transaction do
+          cn.transaction do
+            # ===== هنا تضيف كود معالجة الرسائل مباشرة =====
+
+            # مثال بسيط: أمر تسجيل
+            if !bd[message.from.id] && !@config["bban"].include?(message.from.id)
+              case message.text
+              when "#تسجيل"
+                unless db[message.from.id]
+                  db[message.from.id] = {
+                    "Power" => 150,
+                    "Defanse" => 100,
+                    "res" => 600,
+                    "Gems" => 60,
+                    "Attacks" => 0,
+                    "Wins" => 0,
+                    "Loses" => 0,
+                    "Defanse_a" => 0,
+                    "dd_win" => 0,
+                    "dd_loses" => 0,
+                    "level" => 1,
+                    "Shield" => true,
+                    "clan" => "none"
+                  }
+                  bot.api.send_message(chat_id: message.chat.id, text: "تم تسجيلك بنجاح ⚔")
+                  puts "#{message.from.username} HAS BEEN SIGNED UP".on_green
+                else
+                  bot.api.send_message(chat_id: message.chat.id, text: "لقد قمت بالتسجيل مسبقاً ⚔")
+                end
+              end
+            end
+
+            # ===== يمكنك هنا إضافة المزيد من أوامر اللعبة بنفس النمط =====
+
+          end
+        end
+      end
+    end
+  rescue Telegram::Bot::Exceptions::ResponseError => e
+    puts "[Telegram Error] #{e.message}".on_red
+    retry
+  rescue => e
+    puts "[General Error] #{e.message}".on_red
+    sleep 5
+    retry
+  end
 end
 
-
-#designed by Humam Muhammed
-#TELEGRAM_BOT_POOL_SIZE=16 ruby bot.rb
+# Designed by Humam Muhammed
+# Usage: TELEGRAM_BOT_POOL_SIZE=16 ruby bot.rb
